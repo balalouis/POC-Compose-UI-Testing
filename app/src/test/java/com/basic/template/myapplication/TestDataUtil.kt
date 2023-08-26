6b1c14df3f220890e8e0da34d607da15785cc595
Balamurugan Selvam <balan.job@gmail.com>
2023-08-26 11:46:14 +0530

    Dummy data in Test utils

---
+++ app/src/test/java/com/basic/template/myapplication/TestDataUtil.kt
@@ -0,0 +1,8 @@
+package com.basic.template.myapplication
+
+import com.basic.template.myapplication.model.LoginRequestModel
+import com.basic.template.myapplication.model.LoginResponseModel
+
+object TestDataUtil {
+    fun getLoginResponseModel() = LoginResponseModel(token = "assccfgvvhnjmkn")
+}
